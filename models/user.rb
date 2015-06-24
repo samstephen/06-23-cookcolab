26947f4b39ae4af13b45156046553d46c3ab5aed
Sam Stephen <samueljstephen@gmail.com>
2015-06-24 13:27:19 -0500

    Made User class, and initialize.

---
+++ models/user.rb
@@ -0,0 +1,32 @@
+require_relative "database_class_methods.rb"
+require_relative "database_instance_methods.rb"
+
+class User
+  extend DatabaseClassMethods
+  include DatabaseInstanceMethods
+  
+  attr_reader :id
+  attr_accessor :name
+  
+  # Initializes a new user object.
+  #
+  # user_options - Hash containing key/values.
+  #   - id (optional)   - Integer of the user record in the 'users' table.
+  #   - name (optional) - String of the user's name.
+  #
+  # Examples:
+  #
+  #   User.new({ "id" => 1, "name" => "Sam Stephen" })
+  #   => #<User:f32o23424> 
+  #
+  # Returns a User object.
+  def initialize(options={})
+    @id = user_options["id"]
+    @name = user_options["name"]
+  end
+  
+
+
+end
+
+
